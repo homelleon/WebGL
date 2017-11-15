c0b71cb65d7a1243c3315ff4a2c232c0b781769d
homelleon <ihomelleoni@yandex.ru>
2017-11-15 11:15:57 +0300

    second step done

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,20 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const PATHS = {
+	source: path.join(__dirname, 'source'),
+	build: path.join(__dirname, 'build')
+};
+
 module.exports = {
   entry: './app.js',
   output: {
-    filename: 'bundle.js'
-  }
+	path: PATHS.build,
+    filename: '[name].js'
+  },
+  plugins: [
+	  new HtmlWebpackPlugin({
+		  title: 'WebGL testing'
+	  })
+  ]
 };
