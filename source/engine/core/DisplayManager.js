23c7f944f35c36038359f7e97443cd90557f6696
homelleon <ihomelleoni@yandex.ru>
2017-11-15 15:17:00 +0300

    [evironment] add webpack-dev-server

--- source/engine/core/DisplayManager.js
+++ source/engine/core/DisplayManager.js
@@ -14,7 +14,7 @@ export default function Display() {
 	}
 	
 	if(gl) {		
-		gl.clearColor(1.0, 1.0, 0.0, 1.0);
+		gl.clearColor(1.0, 0.0, 0.0, 1.0);
 		gl.clear(gl.COLOR_BUFFER_BIT);
 	}
 	return gl;
